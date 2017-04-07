ae5a3780818af8297598808b55ac2875992a8200
Adolfo R. Brandes <adolfo@hastexo.com>
2017-04-07 19:19:08 -0300

    Add auto_enroll_email()
    Adapt the Instructor Dashboard's email invitation logic for this
    webhook's needs, in the form of an "auto_enroll_email()" function.

--- edx_shopify/utils.py
+++ edx_shopify/utils.py
@@ -1,6 +1,42 @@
 import hashlib, base64, hmac
 
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
+from courseware.courses import get_course_by_id
+from lms.djangoapps.instructor.enrollment import (
+    get_user_email_language,
+    enroll_email,
+    get_email_params
+)
+
 def hmac_is_valid(key, msg, hmac_to_verify):
     hash = hmac.new(key, msg, hashlib.sha256)
     hmac_calculated = base64.b64encode(hash.digest())
     return hmac_calculated == hmac_to_verify
+
+def auto_enroll_email(course_id, email):
+    """
+    Auto-enroll email in course.
+
+    Based on lms.djangoapps.instructor.views.api.students_update_enrollment()
+    """
+    course_id = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+    course = get_course_by_id(course_id)
+    email_params = get_email_params(course, True, secure=True)
+
+    # Raises ValidationError if invalid
+    validate_email(email)
+
+    # Try to find out what language to send the email in.
+    user = None
+    language = None
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        pass
+    else:
+        language = get_user_email_language(user)
+
+    # Enroll the email
+    enroll_email(course_id, email, True, True, email_params, language=language)
