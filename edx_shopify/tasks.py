62bd3281eccf9f32a1f2f384cbef243a32706b7a
Florian Haas <florian@hastexo.com>
2017-05-15 22:46:29 +0200

    Add meaningful logging in tasks.py
    Fixes #4.

--- edx_shopify/tasks.py
+++ edx_shopify/tasks.py
@@ -13,10 +13,13 @@ class ProcessOrder(Task):
 
     """
     def run(self, data):
+        logger.debug('Processing order data: %s' % data)
         order = Order.objects.get(id=data['id'])
 
         # If the order is anything but UNPROCESSED, abandon the attempt.
         if order.status != Order.UNPROCESSED:
+            logger.warning('Order %s has already '
+                           'been processed, ignoring' % order.id)
             return
 
         # Mark the order as being processed.
@@ -26,6 +29,7 @@ class ProcessOrder(Task):
         # Process line items
         order_error = False
         for item in data['line_items']:
+            logger.debug('Processing line item: %s' % item)
             try:
                 sku = item['sku']
                 email = next(
@@ -34,6 +38,8 @@ class ProcessOrder(Task):
                 )
             except (KeyError, StopIteration):
                 order_error = True
+                logger.error('Malformed line item %s in order %s, '
+                             'unable to process' % (item, order.id))
                 continue
 
             # Store line item
@@ -48,6 +54,8 @@ class ProcessOrder(Task):
                     # Enroll the email in the course
                     auto_enroll_email(sku, email)
                 except:
+                    logger.error('Unable to enroll '
+                                 '%s in %s' % (email, sku))
                     order_error = True
                     order_item.status = OrderItem.ERROR
                     order_item.save()
@@ -56,6 +64,8 @@ class ProcessOrder(Task):
                 # Mark the item as processed
                 order_item.status = OrderItem.PROCESSED
                 order_item.save()
+                logger.debug('Successfully processed line item '
+                             '%s for order %s' % (item, order.id))
 
             elif order_item.status == OrderItem.ERROR:
                 order_error = True
@@ -63,6 +73,10 @@ class ProcessOrder(Task):
         # Mark the order status
         if order_error:
             order.status = Order.ERROR
+            logger.error('Failed to fully '
+                         'process order %s' % order.id)
         else:
             order.status = Order.PROCESSED
+            logger.error('Successfully processed '
+                         'order %s' % order.id)
         order.save()
