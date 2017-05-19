8fb6a000bf24127f4227a76a026c082c03a9bb44
Florian Haas <florian@hastexo.com>
2017-05-19 23:20:40 +0200

    auto_enroll_email: move email validation ahead of course lookup
    It's unnecessary to hit the content store if we already know the email
    address isn't valid, so move the call to validate_email() ahead of the
    course lookup.

--- edx_shopify/utils.py
+++ edx_shopify/utils.py
@@ -38,12 +38,12 @@ def auto_enroll_email(course_id, email):
 
     Based on lms.djangoapps.instructor.views.api.students_update_enrollment()
     """
+    # Raises ValidationError if invalid
+    validate_email(email)
+
     course_id = SlashSeparatedCourseKey.from_deprecated_string(course_id)
     course = get_course_by_id(course_id)
     email_params = get_email_params(course, True, secure=True)
-
-    # Raises ValidationError if invalid
-    validate_email(email)
 
     # Try to find out what language to send the email in.
     user = None
